510bc5f7fb45388064cbb0f69b04934825f15fc1
Blest <blest@Hansons-MacBook-Pro.local>
2024-07-08 23:47:29 -0500

    added dashboard symbol on nav bar and added user icon on nav bar. Rremoving user button from home page

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
 
-publicRoutes: ["/api/webhooks(.*)"]
-
+  publicRoutes:
+    ["/",
+      "/api/webhooks(.*)"]
 });
 
 
