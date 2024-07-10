224d5bf216ffe7cea64ff18fff75c53f3410f3b4
Blest <blest@Hansons-MacBook-Pro.local>
2024-07-10 00:29:19 -0500

    User can collasped and expand the side bar

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -3,7 +3,7 @@
 import { useSidebar } from "@/store/use-sidebar"
 import { Button } from "@/components/ui/button";
 
-import { ArrowLeftFromLineIcon } from "lucide-react";
+import { ArrowLeftFromLineIcon, ArrowRightFromLine } from "lucide-react";
 
 
 export const Toggle = () => {
@@ -18,6 +18,16 @@ export const Toggle = () => {
 
     return (
         <>
+            {collapsed && (
+                <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4" >
+                    <Button
+                        onClick={onExpand}
+                        variant="ghost"
+                        className="h-auto p-2">
+                        <ArrowRightFromLine className="h-4 w-4" />
+                    </Button>
+                </div>
+            )}
             {!collapsed && (
                 <div className="p-3 pl-6 mb-2 flex items-center w-full">
                     <p className="font-semibold text-primary" >
