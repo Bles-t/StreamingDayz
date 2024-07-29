3531ff3a86cbab74e1e9a1ee59e281d0c0892354
Blest <blest@Hansons-MacBook-Pro.local>
2024-07-29 18:51:01 -0500

    everytime i save it auto updates the prisma schema .

---
+++ app/(browse)/(home)/[username]/page.tsx
@@ -0,0 +1,22 @@
+interface UserPageProps {
+    params: {
+        username: string;
+    };
+};
+
+
+
+
+
+
+const UserPage = ({
+    params
+}: UserPageProps) => {
+    return (
+        <div>
+            User:   {params.username}
+        </div>
+    );
+};
+
+export default UserPage;
