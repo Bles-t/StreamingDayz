3fa71260a82cde285b547e8c6bed45ace75de04e
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2024-08-07 23:23:37 -0500

    Added the code and changes from git hub from main branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {
 } from '@clerk/nextjs'
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
             forcedTheme="dark"
             storageKey="streamingdayz-theme `1"
           >
+            <Toaster theme="light" position="bottom-center" />
             {children}
           </ThemeProvider>
         </body>
